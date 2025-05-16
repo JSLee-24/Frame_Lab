c0d13ae5ba30a731e00a5408fa85064c3f362de8
Jade Lee <jadelee0924@gmail.com>
2025-05-15 19:34:57 -0500

    Use threads to handle commands

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
+import threading
 import numpy as np
 import cv2
-from img_utils import save_image
-from video_utils import start_video_record, end_video_record
+from img_utils import save_image, delete_image
+from video_utils import start_video_record, end_video_record, delete_video
 from filters import FILTERS, apply_filter
 
 recording = False
 filtered = False
 filter_code = None
 vid = None
+command = None
 
 web_cam = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-while True: 
-    ret, frame = web_cam.read()
-
-    if not filtered: 
-        cv2.imshow("frame", frame)
-
-    else: 
-        image = apply_filter(filter_code, frame)
-        cv2.imshow("frame", image)
-
-    if recording and vid: 
-        vid.write(frame)
-
-    key = cv2.waitKey(1)
-
-    if key == ord('p'): 
-        # take a photo
+def handle_snapshot(frame):
+    try: 
+        # take a snapshot
         save_image(frame)
-    
-    elif key == ord('r'): 
+
+    except Exception as e: 
+        print(f"Error: {e}")
+
+def handle_record(frame):
+    try: 
+        global fourcc
+
         # record video
         if not recording: 
             vid = start_video_record(frame, fourcc)
@@ -45,7 +39,30 @@ while True:
                 end_video_record(vid)
                 vid = None
 
-    elif key == ord('f'): 
+    except Exception as e: 
+        print(f"Error: {e}")
+
+def handle_delete(): 
+    try: 
+        file_type = input("What would you like to delete?\n(I)mage\n(V)ideo\n>").upper
+
+        if file_type == 'I': 
+            delete_image()
+
+        elif file_type == 'V': 
+            delete_video()
+
+        else: 
+            print("Invalid file type")
+
+    except Exception as e: 
+        print(f"Error: {e}")
+
+def handle_filter():
+    try: 
+        global filtered
+        global filter_code
+
         if filtered: 
             filtered = False
             filter_code = None
@@ -72,6 +89,45 @@ while True:
 
             else: 
                 print("Invalid filter option.")
+
+    except Exception as e: 
+        print(f"Error: {e}")
+        
+print("Welcome to Frame Lab")
+print("""Welcome to Frame Lab!
+What would you like to do?\n
+(S) Take a snapshot\n
+(R) Record a video\n
+(D) Delete a file\n
+(F) Apply a filter\n
+(Q) Quit""")
+
+while True: 
+    ret, frame = web_cam.read()
+
+    if not filtered: 
+        cv2.imshow("frame", frame)
+
+    else: 
+        image = apply_filter(filter_code, frame)
+        cv2.imshow("frame", image)
+
+    if recording and vid: 
+        vid.write(frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('s'): 
+        threading.Thread(target=handle_snapshot, args=(frame,), daemon=True).start()
+    
+    elif key == ord('r'): 
+        threading.Thread(target=handle_record, args=(frame,), daemon=True).start()
+
+    elif key == ord('d'):
+        threading.Thread(target=handle_delete, daemon=True).start()
+
+    elif key == ord('f'): 
+        threading.Thread(target=handle_filter, daemon=True).start()
 
     elif key == ord('q'): 
         break
